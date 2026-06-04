08c2450135ed2ca950e0144bd7a648109dcde958
Christina Kay <ckay314@gmail.com>
2026-06-04 17:08:52 -0400

    Sorting params to axes and basic ht plot seems to be working

--- wombatPlots.py
+++ wombatPlots.py
@@ -24,8 +24,21 @@ types:
 '''
 
 #|--- Standard input error message ---|
-global errorStrings
+global errorStrings, labSwap, labelMatch, dubColor
 errorStrings = [' ', '  python3 wombatPlots.py logFile id(s) type otherParams', '          where:', '          - logFile is a wombat log file', '          - ids is an integer or int+int or int-int', '          - type sets the plot type from [ht, kin, en, linimg, logimg, sqimg]', '          - otherParams includes min#, max#, outName, and picType']
+
+# Dictionary to swap from the GUI names that dont like latex to
+# nicer things for this plot
+deg = '($^{\\circ}$)'
+labSwap = {'Height (Rs)':'Height (R$_S$)', 'Lon (deg)':'Lon'+deg , 'Lat (deg)': 'Lat'+deg, 'Tilt (deg)':'Tilt'+deg, 'AW (deg)':'AW'+deg, 'kappa':'$\\kappa$', 'AW_FO (deg)':'AW$_{FO}$'+deg, 'AW_EO (deg)':'AW$_{EO}$'+deg, 'deltaAx':'$\\delta_{Ax}$', 'deltaCS':'$\\delta_{CS}$', 'ecc1':'$\\epsilon_1$', 'ecc2':'$\\epsilon_2$', 'Roll (deg)':'Roll'+deg, 'Yaw (deg)':'Yaw'+deg, 'Pitch (deg)':'Pitch'+deg, 'Lx (Rs)':'L$_x$ (R$_S$)', 'Ly (Rs)':'L$_y$ (R$_S$)', 'Lz (Rs)':'L$_z$ (R$_S$)', 'HeightO (Rs)':'Height$_O$ (R$_S$)', 'LonO (deg)':'Lon$_O$'+deg, 'LatO (deg)':'Lat$_O$'+deg}
+
+
+# Who to pair things with, GCS* is longest, followed by Slab
+# Only match nice pairs, otherwise let it dump them in wherever
+labelMatch = {'Tilt (deg)':['Roll (deg)'], 'AW (deg)': ['AW_FO (deg)', 'Lx (Rs)'], 'AW_FO (deg)':['AW (deg)', 'Lx (Rs)'],  'AW_EO (deg)':['AW (deg)', 'Ly (Rs)'], 'kappa':['ecc1', 'deltaAx',  'Yaw (deg)'], 'ecc1':['kappa', 'deltaAx', 'Yaw (deg)'],  'ecc2': ['deltaCS', 'Pitch (deg)'], 'deltaAx':['kappa', 'ecc1', 'Yaw (deg)'], 'deltaCS':['ecc2', 'Pitch (deg)']}
+
+#dubColor = '#762b99'  # color for right labels used by two+ wfs
+
 
 # |-----------------------------|
 # |--- Process Required Args ---|
@@ -144,7 +157,7 @@ def processArgs(args):
         wombatRes[aInst] = {}
         for aWF in myWFs:
             if aWF not in wfTypes:
-                wfTypes.append(aWF)
+                wfTypes.append(str(aWF))
             # Check that it is a single pickle for this inst/wf shape
             myIds = someIdx[np.where(miniLog[someIdx,3] == aWF)[0]]
             # Collect the parameters
@@ -199,96 +212,6 @@ def profilePlot(mode, wombatRes, wfTypes, logH=False, wfColors=False):
         kin1, kin2, kin3 - same as ht but adds vel + accel
         en1, en2, en3   - same as kin but add mass + KE
     '''
-    #|----------------------------|
-    #|--- Set up based on mode ---|     
-    #|----------------------------|
-    
-    yLabelsL = []
-    id2id    = {} # axis number for each param
-    yLabelsR = []
-    id2idR   = {}
-    rAxes    = []
-    
-    # Only taking height from fit
-    if '1' in mode:
-        nParams = 1
-        tempWF =  wf.wireframe(wfTypes[0].replace('Half', 'Half '))
-        yLabelsL.append(tempWF.labels[0])
-        for wft in wfTypes:
-            id2id[wft] = [0]
-        
-    
-    # Only height and ang width
-    elif '2' in mode:
-        nParams = 1
-        # Run quick check to see all the aws before sorting
-        myAWs = []
-        for wft in wfTypes:
-            tempWF = wf.wireframe(wft.replace('Half', 'Half '))
-            if 'AW (deg)' in tempWF.labels:
-                myAWs.append('AW (deg)')
-            if 'AW_FO (deg)' in tempWF.labels:
-                myAWs.append('AW_FO (deg)')
-            if 'AW_EO (deg)' in tempWF.labels:
-                myAWs.append('AW_EO (deg)')
-        nAWs = len(np.unique(myAWs))
-        # 1 aw - simple case (test with wbOutputs/201207.txt 95-109 ht2 )
-        if nAWs == 1:
-            print ('hi')
-        
-        # 2 aw - only have torus fo/eo but not aw (wbOutputs/WomBlog.txt 95+98+101 ht2)
-        if nAWs == 2:
-            print ('hi2')
-            
-        # 3 aw - have both aw and fo/eo (wbOutputs/WomBlog.txt 95-109 ht2)
-        if nAWs == 3:
-            print ('h3')
-        
-    # All the parameters -> pull what was done before from below    
-    else:
-        nParams = 0
-        maxType = None
-        for wft in wfTypes:
-            if wf.npDict[wft] > nparams:
-                nparams = wf.npDict[wft]
-                maxType = wft
-    
-    
-    print (yLabelsL)
-    print (sd)
-   
-   
-   
-   
-   
-   
-   
-    fig, ax = plt.subplots(nparams, 1, figsize=(7,nparams), layout='constrained')
-    ylabs = {}
-    for wft in wfTypes:
-        tempWF = wf.wireframe(wft)
-        ylabs[wft] = tempWF.labels
-        
-    # Make mapping from wf labels order to axes order
-    ind2ind = {}
-    ind2ind[maxType] = range(nparams)
-    mainlabs = np.array(ylabs[maxType])
-    for wft in wfTypes:
-        if wft != maxType:
-            myn = len(ylabs[wft])
-            myorder = np.empty(myn)
-            for i in range(myn):
-                if (ylabs[wft][i] in mainlabs):
-                    idx = np.where(mainlabs ==  ylabs[wft][i])[0]
-                    myorder[i] = idx[0]
-                else:
-                    sys.exit('Non matchin params, need to code thuis')
-            ind2ind[wft] = myorder.astype(int)
-        
-    for i in range(nparams):
-        ax[i].set_ylabel(ylabs[maxType][i])
-    # Do  alt axes if needed
-    
     #|---------------------|
     #|--- Set up colors ---|     
     #|---------------------|
@@ -302,13 +225,232 @@ def profilePlot(mode, wombatRes, wfTypes, logH=False, wfColors=False):
         for wft in wfTypes:
             pltColors[wft] = cols[counter]
             counter += 1
-        
+
+    #|----------------------------|
+    #|--- Set up based on mode ---|     
+    #|----------------------------|
+    # Figure out what we are plotting and sort things
+    # by collecting into a list of ylabels to show on the
+    # left and additional right ylabels (if needed)
     
-    #|-----------------------|
-    #|--- Plot the things ---|     
-    #|-----------------------|
-    myInsts = wombatRes.keys()
-    hasLabel = []
+    yLabelsL = []
+    id2id    = {} # axis number for each param i2i[param] = ax
+    yLabelsR = []
+    id2idR   = {}
+    rAxes    = []
+    rColors  = {} 
+    lColors   = {}
+    lColors[0] = 'k' # everyone has a height
+    
+    # |--- Case 1 ---|
+    # Only height 
+    if '1' in mode:
+        nParams = 1
+        tempWF =  wf.wireframe(wfTypes[0].replace('Half', 'Half '))
+        yLabelsL.append(tempWF.labels[0])
+        for wft in wfTypes:
+            id2id[wft] = [0]
+        
+    # |--- Case 2 ---|
+    # Only height and ang width
+    elif '2' in mode:
+        nParams = 1
+        # Loop to see what combo of AWs we have
+        myAWs = []
+        for wft in wfTypes:
+            tempWF = wf.wireframe(wft.replace('Half', 'Half '))
+            if 'AW (deg)' in tempWF.labels:
+                lColor = pltColors[wft]
+                myAWs.append('AW (deg)')
+            if 'AW_FO (deg)' in tempWF.labels:
+                myAWs.append('AW_FO (deg)')
+            if 'AW_EO (deg)' in tempWF.labels:
+                myAWs.append('AW_EO (deg)')
+            # set up the index mapping length, set -1 as default    
+            id2id[wft] = np.zeros(len(tempWF.labels), dtype=int) - 1
+            id2idR[wft] = np.zeros(len(tempWF.labels), dtype=int) - 1
+        
+        # Add the height for everyone        
+        yLabelsL.append('Height (Rs)')
+        for wft in wfTypes:
+            id2id[wft][0] = 0
+                
+        # Process based on number of AW tags        
+        nAWs = len(np.unique(myAWs))
+        # 1 aw - simple case (test with wbOutputs/201207.txt 95-109 ht2 )
+        if nAWs == 1:
+            nParams = 2
+            yLabelsL.append('AW (deg)')
+            lColors[1] = 'k'
+            for wft in wfTypes:
+                tempWF = wf.wireframe(wft.replace('Half', 'Half '))
+                if 'AW (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW (deg)')[0]
+                    id2id[wft][idx[0]] = 1
+        
+        # 2 aw - only have torus fo/eo but not aw (wbOutputs/WomBlog.txt 95+98+101 ht2)
+        if nAWs == 2:
+            nParams = 3
+            yLabelsL.append('AW_FO (deg)')
+            yLabelsL.append('AW_EO (deg)')
+            
+            for wft in wfTypes:
+                tempWF = wf.wireframe(wft.replace('Half', 'Half '))
+                if 'AW_FO (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW_FO (deg)')[0]
+                    id2id[wft][idx[0]] = 1
+                    if len(wfTypes) > 1:
+                        lColors[1] = pltColors[wft]
+                    else:
+                        lColors[1] = 'k'
+                if 'AW_EO (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW_EO (deg)')[0]
+                    id2id[wft][idx[0]] = 2
+                    if len(wfTypes) > 1:
+                        lColors[2] = pltColors[wft]
+                    else:
+                        lColors[2] = 'k'
+            
+        # 3 aw - have both aw and fo/eo (wbOutputs/WomBlog.txt 95-109 ht2)
+        if nAWs == 3:
+            nParams = 3
+            yLabelsL.append('AW (deg)')
+            yLabelsL.append('')
+            yLabelsR.append('')
+            yLabelsR.append('AW_FO (deg)')
+            yLabelsR.append('AW_EO (deg)')
+            for wft in wfTypes:
+                tempWF = wf.wireframe(wft.replace('Half', 'Half '))
+                if 'AW (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW (deg)')[0]
+                    id2id[wft][idx[0]] = 1
+                    lColors[1] = pltColors[wft]
+                if 'AW_FO (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW_FO (deg)')[0]
+                    id2idR[wft][idx[0]] = 1
+                    rColors[1] = pltColors[wft]
+                if 'AW_EO (deg)' in tempWF.labels:
+                    idx = np.where(tempWF.labels == 'AW_EO (deg)')[0]
+                    id2idR[wft][idx[0]] = 2
+                    rColors[2] = pltColors[wft]
+                
+        
+    # |--- Case 3 ---|
+    # Plot it all!
+    else:
+        # Figure out who has the most params and
+        # what that number is. Collect the labels
+        # at the same time
+        nParams = 0
+        maxType = None
+        ylabs = {}
+        for wft in wfTypes:
+            if wf.npDict[wft.replace('Half', 'Half ')] > nParams:
+                nParams = wf.npDict[wft.replace('Half', 'Half ')]
+                maxType = wft
+            tempWF = wf.wireframe(wft.replace('Half', 'Half '))
+            ylabs[wft] = tempWF.labels
+        id2id[maxType] = np.arange(0,nParams)
+        yLabelsL = np.array(ylabs[maxType])
+        # set right labels as long enough flag it string
+        yLabelsR = np.array(['nullnullnullnull' for i in range(nParams)])
+        lColors = {}
+        for i in range(nParams):
+            lColors[i] = pltColors[maxType]
+        doubleLabels = {}
+        doubleColors = {}
+        
+        # Loop through the other WFs and assign to spots
+        for wft in wfTypes:
+            if wft != maxType:
+                myn = len(ylabs[wft])
+                id2id[wft] = []
+                for i in range(myn):
+                    if (ylabs[wft][i] in yLabelsL):
+                        idx = np.where(yLabelsL ==  ylabs[wft][i])[0]
+                        id2id[wft].append(idx[0])
+                    else:
+                        id2id[wft].append(-1) # flag as not on left
+                        # Figure out if we already have rights
+                        if wft not in id2idR.keys():
+                            id2idR[wft] = np.zeros(myn) - 1 # set -1 for empty
+                        # Add to right if label already exits on right    
+                        if (ylabs[wft][i] in yLabelsR):
+                            idx = np.where(yLabelsR ==  ylabs[wft][i])[0]
+                            id2idR[wft][i] = int(idx[0])
+                            
+                        # Otherwise add to right    
+                        if ylabs[wft][i] in labelMatch.keys():
+                            myFriends = labelMatch[ylabs[wft][i]]
+                            idL = -1 # subtle shiver at this var name
+                            lonely = True
+                            while lonely:
+                                for aFriend in myFriends:
+                                    if aFriend in yLabelsL:
+                                        idL = np.where(yLabelsL == aFriend)[0] 
+                                        id2idR[wft][i] = int(idL[0])
+                                        if yLabelsR[idL] == 'nullnullnullnull':
+                                            yLabelsR[idL] = ylabs[wft][i]
+                                            rColors[idL[0]] = pltColors[wft]   
+                                        elif yLabelsR[idL] != ylabs[wft][i]:
+                                        #else:
+                                            doubleLabels[idL[0]] = [yLabelsR[idL[0]], ylabs[wft][i]]
+                                            yLabelsR[idL] = 'double'
+                                            doubleColors[idL[0]] = [rColors[idL[0]], pltColors[wft]]
+                                        lonely = False
+                                if lonely:
+                                        sys.exit(ylabs[wft][i] + ' has no friend, find somewhere to add in labelMatch dictionary')
+                                        
+        # Clean up left labels - set at black if no right axis label
+        for i in range(nParams):
+            if yLabelsR[i] == 'nullnullnullnull':
+                lColors[i] = 'k'
+                               
+    #|-------------------------|
+    #|--- Add in kinematics ---|
+    #|-------------------------|
+    
+    #|-------------------------|
+    #|--- Add in energetics ---|
+    #|-------------------------|
+    
+
+        
+    #|-------------------------|
+    #|--- Set up the figure ---|     
+    #|-------------------------|
+    np2sz = {1:3, 2:5, 3:6, 4:5, 5:6, 6:7, 7:8, 8:9, 9:10, 10:11, 11:12}
+    fig, ax = plt.subplots(nParams, 1, figsize=(7,np2sz[nParams]), layout='constrained')
+    
+    if nParams == 1:
+        ax = [ax]
+        
+    axR = {}
+    
+    #|--- Set up the left labels ---|
+    for i in range(nParams):
+        if yLabelsL[i] != '': 
+            ax[i].set_ylabel(labSwap[yLabelsL[i]], color=lColors[i])
+
+    #|--- Set up the right labels ---|
+    for i in range(len(yLabelsR)):
+        # Single right label
+        if yLabelsR[i] not in ['nullnullnullnull', 'double', '']:
+            nowC = rColors[i]
+            axR[i] = ax[i].twinx() 
+            axR[i].set_ylabel(labSwap[yLabelsR[i]], color=nowC)
+        # Double right label (or more?)
+        elif yLabelsR[i] == 'double':
+            axR[i] = ax[i].twinx() 
+            axR[i].set_ylabel('')
+            for j in range(len(doubleLabels[i])):            
+                axR[i].text(0.98,0.95-0.23*j, labSwap[doubleLabels[i][j]], transform=axR[i].transAxes, horizontalalignment='right', verticalalignment='top', color=doubleColors[i][j] )
+    
+            
+    #|--------------------------|
+    #|--- Fill in the figure ---|     
+    #|--------------------------| 
+    hasLabel = []   
     for aInst in wombatRes.keys():
         subRes = wombatRes[aInst]
         for aType in wfTypes:
@@ -316,22 +458,36 @@ def profilePlot(mode, wombatRes, wfTypes, logH=False, wfColors=False):
                 myRes = subRes[aType]
                 myTimes = myRes['times']
                 myParams = myRes['times']
-                myn = len(ylabs[aType])
                 myC = pltColors[aType]
                 
-                for i in range(myn):
-                    if aType not in hasLabel:
-                        ax[ind2ind[aType][i]].plot(myTimes, myRes['paramGrid'][i,:], 'o', c=myC, label=aType)
-                        hasLabel.append(aType)
-                    else:
-                        ax[ind2ind[aType][i]].plot(myTimes, myRes['paramGrid'][i,:], 'o', c=myC)
+                for i in range(len(id2id[aType])):
+                    myax = None
+                    if id2id[aType][i] != -1:
+                        myax = ax[id2id[aType][i]]
+                    elif aType in id2idR:
+                        if id2idR[aType][i] != -1:
+                            myax = axR[id2idR[aType][i]]
+                                              
+                    if type(myax) != type(None):    
+                        if aType not in hasLabel:
+                            myax.plot(myTimes, myRes['paramGrid'][i,:], 'o', c=myC, label=aType)
+                            hasLabel.append(aType)
+                        else:
+                            myax.plot(myTimes, myRes['paramGrid'][i,:], 'o', c=myC)
     if logH:
         ax[0].set_yscale('log')
                         
     ax[0].legend(loc='lower right', bbox_to_anchor=(1., 1.), ncols=len(hasLabel))
     
-    #print (ylabs)
+        
+        
+    #print (id2id)
+    #print (id2idR)
+    #print (yLabelsL)
+    #print (yLabelsR)
+    
     plt.show()
+
     
 
 def wombatPlotWrapper(args):
